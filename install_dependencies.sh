41b2b8c912bed25d936b826cb44349b2b5bf7718
iguidado <noob.3vol@gmail.com>
2024-05-03 23:49:52 +0200

    Script now work on ubuntu too + remove adding user to ubridge group

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
+
+OS=$(lsb_release -i | cut -d$'\t' -f2 | tr [A-Z] [a-z])
+
+
 # # Install docker
 sudo apt-get update && sudo apt upgrade
 sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/${OS}/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
  
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${OS} \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
@@ -48,7 +53,7 @@ sudo apt install wireshark -y
 
 
 # Enable docker support for GNS3
-for i in ubridge docker wireshark; do
+for i in docker wireshark; do
  sudo usermod -aG $i $USER
 done
 
