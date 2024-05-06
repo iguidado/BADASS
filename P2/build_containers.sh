7595acd7c6c56602b39a57ad3a82d6f181f17717
hugo <hugo@iot>
2024-05-06 13:59:26 +0200

    [UP] added script to multicast

--- P2/build_containers.sh
+++ P2/build_containers.sh
@@ -2,8 +2,8 @@
 
 # docker build ./router -t router-p2-2
 
-docker build ./host -t host-$USER #--build-arg IP_A="30.1.1.1"
-docker build ./router -t router-$USER
+docker build ./host -t host-p2 #--build-arg IP_A="30.1.1.1"
+docker build ./router -t router-p2
 
 #docker rm -f test
 #docker container run --name host-p -d host-p2 
--- P2/router/conf_vxlan.sh
+++ P2/router/conf_vxlan.sh
@@ -9,7 +9,13 @@ fi
 ip link add br0 type bridge
 ip link set dev br0 up
 ip addr add 10.1.1.${HOSTNBR}/24 dev eth0
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.${REMOTE_NBR} local 10.1.1.${HOSTNBR} dstport 4789
+
+if [ ${MULTICAST} -eq 1 ]; then
+	ip link add name vxlan10 type vxlan id 10 dev eth0 group 239.1.1.1 dstport 4789
+else
+	ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.${REMOTE_NBR} local 10.1.1.${HOSTNBR} dstport 4789
+fi
+
 ip addr add 20.1.1.${HOSTNBR}/24 dev vxlan10
 brctl addif br0 eth1
 brctl addif br0 vxlan10
