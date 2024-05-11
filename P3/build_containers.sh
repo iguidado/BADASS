d4e7df4e4deda99943b0dc6174573ac521ff2093
entropy <noob.3vol@gmail.com>
2024-05-11 13:52:11 +0200

    cleaned building script

--- P3/build_containers.sh
+++ P3/build_containers.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# docker build ./router -t router-p2-2
-
-docker build ./host -t host-p3 #--build-arg IP_A="30.1.1.1"
+docker build ./host -t host-p3
 docker build ./router -t router-p3
-
-#docker rm -f test
-#docker container run --name host-p -d host-p2 
